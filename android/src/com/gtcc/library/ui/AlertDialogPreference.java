7f1b8b830b4a5255b42d2d009520dddf14179fd3
tanzhenxin <tanzhenxing1987@gmail.com>
2013-07-12 16:36:26 +0800

    settings

---
+++ android/src/com/gtcc/library/ui/AlertDialogPreference.java
@@ -0,0 +1,32 @@
+package com.gtcc.library.ui;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class AlertDialogPreference extends DialogPreference {
+	
+	public DialogInterface.OnClickListener onClickListener;
+
+	public AlertDialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlertDialogPreference(Context context, AttributeSet attrs,
+			int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	
+	public void setOnClickListener(DialogInterface.OnClickListener listener) {
+		onClickListener = listener;
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		super.onClick();
+		if (onClickListener != null) 
+			onClickListener.onClick(dialog, which);
+	}
+}
--- android/src/com/gtcc/library/ui/SettingsActivity.java
+++ android/src/com/gtcc/library/ui/SettingsActivity.java
@@ -1,18 +1,42 @@
 package com.gtcc.library.ui;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.gtcc.library.R;
 
 public class SettingsActivity extends SherlockPreferenceActivity {
 
+	private AlertDialogPreference logoutPreference;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		addPreferencesFromResource(R.layout.activity_settings);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		logoutPreference = (AlertDialogPreference) findPreference("pref_key_log_out");
+		logoutPreference.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				switch (which) {
+				case DialogInterface.BUTTON_POSITIVE:
+					break;
+				case DialogInterface.BUTTON_NEGATIVE:
+					dialog.dismiss();
+					break;
+				}
+			}
+			
+		});
 	}
+
 
 }
