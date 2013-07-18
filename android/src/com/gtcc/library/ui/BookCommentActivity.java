d2d2baf2137542eadd2386ac3a3b33a9ee4104eb
tanzhenxing <tanzhenxing1987@gmail.com>
2013-07-18 23:47:33 +0800

    null

--- android/src/com/gtcc/library/ui/BookCommentActivity.java
+++ android/src/com/gtcc/library/ui/BookCommentActivity.java
@@ -1,6 +1,8 @@
 package com.gtcc.library.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
@@ -9,12 +11,17 @@ import com.gtcc.library.R;
 
 public class BookCommentActivity extends SherlockActivity {
 	
-	public static final String BOOK_TITLE = "title"; 
+	public static final String BOOK_TITLE = "title";
+	public static final String BOOK_COMMENT = "comment";
+	
+	private EditText mEditText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comment);
+		
+		mEditText = (EditText) findViewById(android.R.id.text1);
 		
 		String bookTitle = getIntent().getExtras().getString(BOOK_TITLE);
 
@@ -34,6 +41,11 @@ public class BookCommentActivity extends SherlockActivity {
 		case android.R.id.home:
 			finish();
 			return true;
+		case R.id.send_review:
+			Intent intent = new Intent();
+			intent.putExtra(BOOK_COMMENT, mEditText.getText().toString());
+			setResult(RESULT_OK, intent);
+			finish();
 		default:
 			return false;
 		}
